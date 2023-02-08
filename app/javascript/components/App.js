c0a02ce31a85f98485e318d951d2bc7b036d4fa4
dgfarm02 <dgfarm2@gmail.com>
2023-02-08 14:09:47 -0800

    completed the unprotected index card

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-
 import ApartmentEdit from "./pages/ApartmentEdit"
 import ApartmentIndex from "./pages/ApartmentIndex"
 import ApartmentNew from "./pages/ApartmentNew"
 import ApartmentShow from "./pages/ApartmentShow"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
+import mockApartments from "./mockApartments"
+
 
 const App = (props) => {
-  const [apartments, setApartments] = useState([])
-
+  const [apartments, setApartments] = useState(mockApartments)
+  console.log(apartments);
   useEffect(() => {
     readApartments()
   }, [])
@@ -32,7 +32,7 @@ const App = (props) => {
       <Header {...props} />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/apartmentindex" element={<ApartmentIndex />} />
+        <Route path="/apartmentindex" element={<ApartmentIndex apartments={apartments}/>} />
         <Route path="/apartmentshow" element={<ApartmentShow />} />
         <Route path="/apartmentnew" element={<ApartmentNew />} />
         <Route path="/apartmentedit" element={<ApartmentEdit />} />
--- app/javascript/components/pages/ApartmentIndex.test.js
+++ app/javascript/components/pages/ApartmentIndex.test.js
@@ -1,10 +1,25 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import ApartmentIndex from "./ApartmentIndex"
+import mockApartments from "../mockApartments"
+import { BrowserRouter } from "react-router-dom";
 
 describe("<ApartmentIndex />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
     render(<ApartmentIndex />, div)
+  })
+  it("renders apartment cards", () => {
+    const div = document.createElement("div")
+    render(
+      // <BrowserRouter>
+      <ApartmentIndex apartment={mockApartments} />, div )
+      {/* </BrowserRouter>, div) */}
+      // mockApartments.forEach(apartment => {
+      //   const apartmentPrice = screen.getByText(apartment.price)
+      //   expect(apartmentPrice).toBeInTheDocument() 
+      // })
+      const apartmentText = screen.getByText("Apartments")
+      expect(apartmentText).toBeInTheDocument()
   })
 })
