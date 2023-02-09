1b5f5c7978ad703a1881e02685a7151b71819f4e
nvencer98 <vencer98@gmail.com>
2023-02-09 13:30:05 -0800

    create endpoint done

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,7 +1,16 @@
 class ApartmentsController < ApplicationController
 
-def index
-    apartments = Apartment.all
-    render json: apartments
+    def index
+        apartments = Apartment.all
+        render json: apartments
+    end
+
+    def create
+        apartment = Apartment.create(apartment_params)
+        render json: apartment
+    end
+    private
+    def apartment_params
+        params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
     end
 end
